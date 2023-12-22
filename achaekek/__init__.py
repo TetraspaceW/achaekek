db9e0452f3e201cef0829b7708e34279322478fa
TetraspaceW <holomanga@gmail.com>
2023-12-22 21:30:10 +0000

    Add optional params to CreateMarket class

--- achaekek/__init__.py
+++ achaekek/__init__.py
@@ -1,6 +1,7 @@
 import requests
 from enum import Enum
 from datetime import datetime
+import time
 from dataclasses import dataclass, field
 
 
@@ -12,27 +13,55 @@ class OutcomeType(Enum):
     BOUNTIED_QUESTION = "BOUNTIED_QUESTION"
 
 
+class Visibility(Enum):
+    PUBLIC = "public"
+    UNLISTED = "unlisted"
+
+
+class DescriptionFormat(Enum):
+    HTML = "descriptionHTML"
+    MARKDOWN = "descriptionMarkdown"
+    JSON = "descriptionJSON"
+
+
 @dataclass(kw_only=True)
-class CreateMarket:
+class _CreateMarket:
     question: str
     closeTime: datetime = None
+    description: str | tuple[str, DescriptionFormat] = None
+    visibility: Visibility = None
+    groupIds: list[str] = None
+    extraLiquidity: int = None
 
     def to_json(self):
+        """
+        Converts the create market request dataclass to a JSON dictionary suitable for sending as a request to the Manifold API, reformatting the close time and description as needed.
+
+        Returns
+        -------
+        json: dict[str, Any]
+            The JSON dictionary to be sent as a request.
+        """
         json = {k: v for k, v in self.__dict__.items() if v is not None}
         if "outcomeType" in json:
-            json["outcomeType"] = json["outcomeType"].value
+            json["outcomeType"] = self.outcomeType.value
+        if "closeTime" in json:
+            json["closeTime"] = int(time.mktime(self.closeTime.timetuple()) * 1000)
+        if "description" in json and isinstance(self.description, tuple):
+            json[self.description[1].value] = self.description[0]
+            del json["description"]
 
         return json
 
 
 @dataclass
-class CreateBinaryMarket(CreateMarket):
+class CreateBinaryMarket(_CreateMarket):
     initialProb: int
     outcomeType: OutcomeType = field(default=OutcomeType.BINARY)
 
 
 @dataclass
-class CreatePseudoNumericMarket(CreateMarket):
+class CreatePseudoNumericMarket(_CreateMarket):
     question: str
     min: float
     max: float
@@ -48,7 +77,7 @@ class AddAnswersMode(Enum):
 
 
 @dataclass
-class CreateMultipleChoiceMarket(CreateMarket):
+class CreateMultipleChoiceMarket(_CreateMarket):
     question: str
     answers: list[str]
     addAnswersMode: AddAnswersMode
@@ -62,13 +91,13 @@ class CreateMultipleChoiceMarket(CreateMarket):
 
 
 @dataclass
-class CreatePollMarket(CreateMarket):
+class CreatePollMarket(_CreateMarket):
     answers: list[str]
     outcomeType: OutcomeType = field(default=OutcomeType.POLL)
 
 
 @dataclass
-class CreateBountiedQuestionMarket(CreateMarket):
+class CreateBountiedQuestionMarket(_CreateMarket):
     totalBounty: int
     outcomeType: OutcomeType = field(default=OutcomeType.BOUNTIED_QUESTION)
 
@@ -84,23 +113,32 @@ CreateMarketRequest = (
 
 class Client:
     def __init__(self, api_key: str):
+        """
+        Creates a new Manifold client with a given API key.
+
+        Parameters
+        ----------
+        api_key : str
+            Your Manifold API key, which can be found at https://manifold.markets/profile.
+        """
         self.api_key = api_key
 
-    def create_market(self, market: CreateMarketRequest):
+    def create_market(self, market: CreateMarketRequest) -> requests.Response:
+        """
+        Posts a request to create a market on Manifold.
+
+        Parameters
+        ----------
+        market : { CreateBinaryMarket, CreatePseudoNumericMarket, CreateMultipleChoiceMarket, CreateBountiedQuestionMarket, CreatePollMarket }
+            The market to create. Can be of any of the types supported by Manifold: binary (0% - 100%), pseudo-numeric (minimum to maximum), multiple choice, bountied question, or a poll question.
+
+        Returns
+        -------
+        requests.Response
+            The response from the Manifold API.
+        """
         return requests.post(
             "https://api.manifold.markets/v0/market",
             json=market.to_json(),
             headers={"Authorization": f"Key {self.api_key}"},
         )
-
-
-if __name__ == "__main__":
-    market = CreateBinaryMarket(
-        question="Will the library that Tetraspace used to create this question be in a releasable state by the end of February?",
-        initialProb=50,
-    )
-    client = Client("")
-    print(market.to_json())
-    response = client.create_market(market)
-    print(response)
-    print(response.json())
